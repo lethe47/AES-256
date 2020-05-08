fb1640651d7a3cd3fccc584159f8a6461b3346fa
lethe <lethe001021@foxmail.com>
2020-05-08 10:03:21 +0800

    添加项目文件。

---
+++ AES-256/test.cpp
@@ -0,0 +1,18 @@
+﻿#include"AES.h"
+#include <iostream>
+
+int main()
+{
+    while (1)
+    {
+        //Key=BF3D6A0B97E473BE03153689E24A075B，可以在加密函数中修改
+        string cin_str;
+        cout << "请输入一串字符：" << endl;
+        cin >> cin_str;
+        string miwen = encrypt(cin_str);
+        cout << "密文：" << miwen << endl;
+        string mingwen = decrypt(miwen);
+        cout << "明文：" << mingwen << endl;
+    }
+}
+
